dc70c38ddc2695cc91cffe52fd3b0b3dd6acb836
Ivan Eduardo Mees <mees.ivan@hotmail.com>
2022-03-28 22:38:49 -0300

    resend

--- Front/usbwebserver/root/IndexTGR/Index.php
+++ Front/usbwebserver/root/IndexTGR/Index.php
@@ -9,7 +9,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,900;1,900&display=swap" rel="stylesheet">
